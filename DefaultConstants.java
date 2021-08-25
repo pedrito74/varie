9242c512010412854c8402b3a9180cf9294aaf97
Pedrito <pedroschneider@hotmail.it>
2021-08-25 14:20:21 +0200

    Update DefaultConstants.java

--- DefaultConstants.java
+++ DefaultConstants.java
@@ -22,7 +22,7 @@ package com.fourflight.WP.XAI.ISMUtilities;
 public interface DefaultConstants {
 
    enum CompareResult {
-      GREATER_THAN, EQUAL, LESS_THAN;
+      GREATER_THAN, EQUAL, LESS_THAN, TREEEEE;
    }
 
    public static String NA = "NA";
